e3112e07c965514e2499f89d5de30e60c8083c4d
Elizabeth Xiong <elizabeth.xiong34@gmail.com>
2024-09-14 16:17:50 -0400

    Edited size of icons and text

---
+++ script.js
@@ -0,0 +1,3 @@
+// use javascript to load static html using fetch()
+
+console.log("hey! I am new to web development and still learning! I am looking for an internship in either front-end or back-end development.")
