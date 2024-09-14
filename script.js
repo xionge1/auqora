c9f6d03bf764e34f7072803a7cdeea6828124c07
Elizabeth Xiong <elizabeth.xiong34@gmail.com>
2024-09-14 19:07:24 -0400

    Edited icon size, broke code up into different html pages

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // use javascript to load static html using fetch()
 
-console.log("hey! I am new to web development and still learning! I am looking for an internship in either front-end or back-end development.")
+console.log("hey! I am new to web development and still learning! I am looking for an internship in either front-end or back-end development.");
+
+
+fetch('navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading the navbar:', error));
+      
